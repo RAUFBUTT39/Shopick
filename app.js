ff93ded8de7faacedd6e880332fea3a0bf1cf8a7
MUHAMMAD RAUF ARSHAD <71685740+RAUFBUTT39@users.noreply.github.com>
2020-12-31 19:17:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,6 @@ app.use(cors());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "client/build")));
-//Anything that doesn't match the above, send back index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,6 +25,12 @@ app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/products", productsRouter);
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client/build")));
+//Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
